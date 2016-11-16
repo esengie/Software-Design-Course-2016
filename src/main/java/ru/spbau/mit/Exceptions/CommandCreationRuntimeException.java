4f229425c6d0846eb762b7c174544e30b43a65f4
Shamil Garifullin <shamil.garifullin@gmail.com>
2016-11-16 18:47:35 +0300

    Revert "Homework 1: CLI"

--- src/main/java/ru/spbau/mit/Exceptions/CommandCreationRuntimeException.java
+++
@@ -1,8 +0,0 @@
-package ru.spbau.mit.Exceptions;
-
-public class CommandCreationRuntimeException extends RuntimeException {
-
-    public CommandCreationRuntimeException(String name, Throwable cause) {
-        super("Cannot create command " + name, cause);
-    }
-}
--- src/main/java/ru/spbau/mit/Exceptions/VariableUndefinedException.java
+++
@@ -1,9 +0,0 @@
-package ru.spbau.mit.Exceptions;
-
-import java.io.IOException;
-
-public class VariableUndefinedException extends IOException {
-    public VariableUndefinedException(String name) {
-        super("Cannot find variable " + name);
-    }
-}
